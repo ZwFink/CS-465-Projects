367e481453e61bb0776bfe4d514811517b72f62f
CalebJEJ <cjj226@nau.edu>
2020-01-20 11:19:42 -0700

    Setup Socket stuff
    Set up some of the code for creating and binding the socket then calling the EchoThread.

--- Project0/echo_server/src/main/java/EchoServer.java
+++ Project0/echo_server/src/main/java/EchoServer.java
@@ -3,11 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.net.*;
+import java.io.*;
 /**
  *
- * @author zane
+ * @author zane, caleb johnson
  */
-public class EchoServer {
-    
+public class EchoServer 
+{
+    public static void main(String[] args)
+    {
+        if (args.length != 1) 
+        {
+            System.out.println("Please give a port number as an argument");
+            System.exit(1);
+        }
+        
+        int port = Integer.parseInt(args[0]);
+        
+        try 
+        {
+            ServerSocket ss = new ServerSocket(port);
+            Socket clientSocket = ss.accept();
+            EchoThread echoT = new Echothread(clientSocket);
+            echoT.run();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Bad port, try again");
+            System.exit(1);
+        }
+    }
 }
