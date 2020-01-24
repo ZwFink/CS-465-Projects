e77783d8d0891592348dfdb5a3d5aacd3fbcb391
tinyturtleballs <47403283+tinyturtleballs@users.noreply.github.com>
2020-01-24 12:45:54 -0700

    State machine

--- Project0/echo_server/src/main/java/MessageState.java
+++ Project0/echo_server/src/main/java/MessageState.java
@@ -5,14 +5,18 @@
  */
 
 /**
- *
  * @author zane
+ * @author kenneth
+ * @author caleb
  */
-public class MessageState 
+public class MessageState
 {
     private char lastChar;
     private CurrentState state;
-    
+
+   /*
+    * Gets the current message state
+    */
     private enum CurrentState
     {
       START_STATE,
@@ -21,67 +25,89 @@ public class MessageState
       QUI_REACHED,
       QUIT_REACHED
     };
-    
+
+   /*
+    * Gets the current message state
+    */
     public MessageState()
     {
         lastChar = '\0';
         state = CurrentState.START_STATE;
     }
-    
+
+   /*
+    * Process each character to determine if
+    * quit has been reached
+    * @param in: The character to test
+    * cycles though inputs until state reaches QUIT_REACHED
+    */
     public void processChar( char in )
     {
-        if( charIsSendable( in ) 
-             && Character.isLowerCase( in )
-          )
+        if( charIsSendable( in ) && Character.isLowerCase( in ))
         {
 
             if( state == CurrentState.START_STATE && in == 'q'  )
             {
                 state = CurrentState.Q_REACHED;
-            }            
+            }
             else if( state == CurrentState.Q_REACHED && in == 'u'  )
             {
                 state = CurrentState.QU_REACHED;
             }
-            
-            
+            else if( state == CurrentState.QU_REACHED && in == 'i'  )
+            {
+                state = CurrentState.QUI_REACHED;
+            }
+            else if( state == CurrentState.QUI_REACHED && in == 't'  )
+            {
+                state = CurrentState.QUIT_REACHED;
+            }
             else
             {
                 state = CurrentState.START_STATE;
             }
-            
+
             lastChar = in;
         }
     }
-    
+
+   /*
+    * @returns true when quit statement reached
+    */
     public boolean quitReached()
     {
         return true;
     }
-   
+
+   /*
+    * @returns the last processed character
+    */
     public char getLastChar()
     {
         return lastChar;
     }
-    
+
+   /*
+    * seperates each character for processing
+    */
     public void clear()
     {
         lastChar = '\0';
     }
-    
+
    /*
-    * Determine whether a character can be sent back to 
-    * the client. 
+    * Determine whether a character can be sent back to
+    * the client.
     * @param test The character to test
-    * @returns true if test is an upper or lower-case 
+    * @returns true if test is an upper or lower-case
     *          letter of the English alphabet.
     */
     public boolean charIsSendable( char test )
     {
         int charInt = (int) test;
-        boolean sendable = ( charInt >= 'a' && charInt <= 'z' )  
+        boolean sendable = ( charInt >= 'a' && charInt <= 'z' )
                 || ( charInt >= 'A' && charInt <= 'Z' );
-        
+
         return sendable;
     }
 }
