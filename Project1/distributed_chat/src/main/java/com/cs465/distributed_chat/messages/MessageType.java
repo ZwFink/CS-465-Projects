5e07fd1c3f5e8c32402c81f120f2f75fe7fa2f03
Bob Costas <finkzane@gmail.com>
2020-02-18 16:18:51 -0700

    added messagetypes enum

--- Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/messages/MessageType.java
+++ Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/messages/MessageType.java
@@ -12,14 +12,45 @@ package com.cs465.distributed_chat.messages;
  */
 public interface MessageType 
 {
-     enum MessageTypes
-    {
-     JOIN_REQUEST,
-     JOIN_RESPONSE,
-     JOIN_NOTIFICATION,
-     CHAT_MESSAGE,
-     LEAVE_MESSAGE
-    }
+	/**
+	 * Enum that implements the 
+	 * different types of messages.
+	 */
+	public enum MessageTypes
+	{
+		/**
+		 * A join request is a message that 
+		 * is sent form a node joining the 
+		 * network to a node that is already on 
+		 * the network.
+		 */
+		JOIN_REQUEST,
+
+		/**
+		 * A join response is the response a node 
+		 * sends to the node from which it received a 
+		 * join request.
+		 */
+		JOIN_RESPONSE,
+
+		/**
+		 * A notification that is sent from a node
+		 * that is joining a network to each other node
+		 * on the network.
+		 */
+		JOIN_NOTIFICATION,
+
+		/**
+		 * A chat message containing a message payload.
+		 */
+		CHAT_MESSAGE,
+		
+		/**
+		 * A message sent by a node that wishes to 
+		 * leave the network.
+		 */
+		LEAVE_MESSAGE
+	}
     
     MessageTypes getType();
 }
