53373385a3bdf321f5efc8e10b5474f30f664f66
Bob Costas <finkzane@gmail.com>
2020-02-18 16:24:29 -0700

    added documentation for NodeInfo class

--- Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/NodeInfo.java
+++ Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/NodeInfo.java
@@ -5,18 +5,41 @@
  */
 package com.cs465.distributed_chat;
 
+import java.io.Serializable;
 import java.net.InetAddress;
 
 /**
- *
+ * The NodeInfo class tracks information that is necessary to connect to 
+ * or display information about any node in the network. A node consists
+ * of an IP address, port number, and logical name.
+ * @note Once set, the information in this class cannot be changed,
+ *       as we assume that information is static during a single chat session. 
  * @author zane
  */
-public class NodeInfo 
+public class NodeInfo implements Serializable
 {
-	private InetAddress IpAddr;
-	private int port;
-	String logicalName;
+	/**
+	 * The IP address of the node. 
+	 */
+	private final InetAddress IpAddr;
 
+	/**
+	 * The port this node can be reached on.
+	 */
+	private final int port;
+
+	/**
+	 * The logical name of the node. Will be displayed 
+	 * when a human-readable representation is needed.
+	 */
+	private final String logicalName;
+
+	/**
+	 * Parameter-initialize information for a node.
+	 * @param ip The IP of the node.
+	 * @param port The port number of the node.
+	 * @param name A logical name for the node.
+	 */
 	public NodeInfo( InetAddress ip,
 			 int port,
 		         String name 
@@ -26,6 +49,33 @@ public class NodeInfo
 		this.port        = port;
 		this.logicalName = name;
 
+	}
+
+	/**
+	 * Get the IP address this node can be reached at.
+	 * @return The IPaddress of the node.
+	 */
+	public InetAddress getIPAddress()
+	{
+		return this.IpAddr;
+	}
+
+	/**
+	 * Get the port this node uses.
+	 * @return the port for this node.
+	 */
+	public int getPort()
+	{
+		return this.port;
+	}
+
+	/**
+	 * Get this node's name.
+	 * @return the name of this node.
+	 */
+	public String getName()
+	{
+		return	this.logicalName;
 	}
 	
 }
