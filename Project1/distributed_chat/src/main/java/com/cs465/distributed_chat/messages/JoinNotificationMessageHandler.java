3bd0b1c09e306b8e7fdd79876a4a8d4c3aa6b954
Bob Costas <finkzane@gmail.com>
2020-03-01 22:14:06 -0700

    added docs for the JoinNotificationMessageHandler class

--- Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/messages/JoinNotificationMessageHandler.java
+++ Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/messages/JoinNotificationMessageHandler.java
@@ -9,19 +9,35 @@ import com.cs465.distributed_chat.NodeInfo;
 import java.util.List;
 
 /**
- *
+ * This class handles received JoinNotification messages.
  * @author zane
  */
 public class JoinNotificationMessageHandler implements MessageHandler 
 {
 	
+	/**
+	 * The memberList of the node that received the JoinNotification
+	 * message.
+	 */
 	List<NodeInfo> memberList;
 
+	/**
+	 * Argument constructor, saves the reference to the node that is 
+	 * joining the network.
+	 * @param membershipList The member list of the node that wishes to 
+	 * join the network.
+	 */
 	public JoinNotificationMessageHandler( List<NodeInfo> membershipList )
 	{
 		this.memberList = membershipList;			
 	}
 
+	/**
+	 * Method to handle the joinNotification message.
+	 * Adds the NodeInfo of the joining node to the member list of the node 
+	 * that received the request.
+	 * @param responseMessage The JoinNotification message that was received.
+	 */
 	@Override
 	public void handleMessage( final MessageType responseMessage )
 	{
