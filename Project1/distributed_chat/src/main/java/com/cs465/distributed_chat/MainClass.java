c6240428d9ef0dc46a0130a7e5fd47987edc50aa
Bob Costas <finkzane@gmail.com>
2020-03-03 03:44:18 -0700

    updated CLI interface

--- Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/MainClass.java
+++ Project1/distributed_chat/src/main/java/com/cs465/distributed_chat/MainClass.java
@@ -10,38 +10,28 @@ import java.lang.Integer;
  */
 public class MainClass 
 {
-   private static int ARGC_CREATE_CHAT = 3;
-   private static int ARGC_JOIN_CHAT = 5;
+   private static int ARGC = 3;
    public static void main(String[] args) throws IOException, InterruptedException 
    {
-       String defaultIP = InetAddress.getLocalHost().toString();
-       int selfPort = 0;
-       InetAddress selfIP = InetAddress.getLocalHost();
-       if( !( args.length == ARGC_CREATE_CHAT 
-	       || args.length == ARGC_JOIN_CHAT 
-	    ) 
-	 )
+       if( args.length != ARGC )
        {
 	    System.out.println("ERROR: Wrong arguments were given. \n\n"
-		    +          "USAGE: (Starting a chat) \n"
+		    +          "USAGE: \n"
 		    + "java -jar jar_name my_ip my_port my_name \n\n"
 		    + "Where jar_name is the name of the jar executable, "
 		    + "my_ip is the IP of the node that is connecting "
-		    + "e.g. 127.0.01., port is the port to use when communicating."
-		    + "\n\nUSAGE: (Joining an existing chat) \n"
-		    + "java -jar jar_name my_ip my_port my_name join_ip join_port \n\n"
-		    + "Where join_ip is the IP of the node to connect to, and "
-		    + "join_port is the port to contact join_ip on.\n\n" 
+		    + "e.g. 127.0.01., port is the port to use when communicating.\n\n"
 	    );
 	    return;
        }
        
        
-       String logicalName = selfIP.getHostName();
-       Node node = new Node(selfIP, selfPort, logicalName);
-       //System.out.println("NODEINFO:" + node);
-       //System.out.println("IP" + selfIP);
-       
+       InetAddress addr = InetAddress.getByName( args[ 0 ] );
+       int port = Integer.parseInt( args[ 1 ] );
+       String logicalName = args[ 2 ];
+
+       Node node = new Node( addr , port, logicalName);
+       node.startChatClient();
    }
    
      
