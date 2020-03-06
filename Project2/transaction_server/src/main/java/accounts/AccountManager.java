95ec2ecd6496481f562243e92c2e6bb07570e6c2
kennyklaw <47403283+kennyklaw@users.noreply.github.com>
2020-03-06 10:56:00 -0700

    added skeleton code

--- Project2/transaction_server/src/main/java/accounts/AccountManager.java
+++ Project2/transaction_server/src/main/java/accounts/AccountManager.java
@@ -11,5 +11,29 @@ package accounts;
  */
 public class AccountManager 
 {
-    // Uses a Lock Manager to handle the reading and writing to an account
+    // Works on the accounts initializes full set of accounts needed
+		
+    //Provides access to accounts give number -> get account
+
+	//Read – Write
+    
+    //Write on → tentative data/committed data
+				//Ex which accounts were read from
+		
+
+    //Shields all the accounts takes high lever read write requests
+        //translates to how read and write are being done.
+
+    //Aware of locking needs to try to acquire a lock
+				//Either in the read or write
+
+    //Write – accnum,  Tran transaction, balance
+         // Variables
+    
+	        //New object with account number
+	        //Lock
+	        //Get balance
+
+    
+    
 }
