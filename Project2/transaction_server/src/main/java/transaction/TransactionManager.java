65624e42cbe18609382dcb5c814da51a7041b054
kennyklaw <47403283+kennyklaw@users.noreply.github.com>
2020-03-06 10:59:25 -0700

    added skeleton code

--- Project2/transaction_server/src/main/java/transaction/TransactionManager.java
+++ Project2/transaction_server/src/main/java/transaction/TransactionManager.java
@@ -12,4 +12,38 @@ package transaction;
 public class TransactionManager 
 {
     // Handles a transaction by calling an Account Manager to handle alterations to the account
+    
+    /*
+    Third object does work in a semantical area deals with the transactions.  
+
+Workers open up one connection then don’t shut it down makes it easier. 
+
+		Request -> open connection and keep it open
+		
+		Reads and writes handled by transaction worker.
+				Threads are used
+
+				Transaction manager extends thread
+					Socket
+					Inputstream	outputstream
+					Message
+			
+					Pass in client start the thread
+
+Take care of read and write requests.
+	Write creates new object
+	Acc num
+		Balance
+	Trans log
+		Balance
+Try - catch
+
+	Read
+		Acc#
+		Transaction
+Try-catch
+
+Finally close all the open sockets read,write,client, Boolean-false
+
+    */
 }
