0a6520a2be7a398b36013a2739f64ac6e55c904c
kennyklaw <47403283+kennyklaw@users.noreply.github.com>
2020-03-09 15:06:44 -0700

    updated comments / added socket connection

--- Project2/transaction_server/src/main/java/client/Client.java
+++ Project2/transaction_server/src/main/java/client/Client.java
@@ -11,9 +11,37 @@ package client;
  * Commands: Open, Read, Write, Close
  * @author caleb
  */
-public class Client 
+public class Client extends thread
 {
+    //save host and port number as variables
+    string host = "127.0.0.1";
+    private final int port = 2080;
+    
     // Opens a proxy server
+    public void run()
+    {
+        // get the transaction id
+        
+        // create loop to handle # of transactions
+        
+        try
+        {
+            //connect to the server given the host and port number
+            Socket socket = new Socket(host, port);
+            
+            //set up read and write streams
+            ObjectInputStream read = new ObjectInputStream(socket.getInputStream);
+            ObjectOutputStream write = new ObjectOutputStream(socket.getInputStream);
+            
+            // handle transactions & proper locks
+                //reading -- viewing account
+                //writing
+                    //Depositing
+                    //Withdrawling
+        }
+    }
+    
+    
         // Issues read and write commands
     // Closes the proxy server
 }
