6c40e959a30cb19ece77ac231489bc1501a9bb6a
CalebJEJ <cjj226@nau.edu>
2020-03-09 10:52:38 -0700

    Got some of the example code from lecture
    Lock class is missing later half of aquire due to not recording it in time, we will have to figure it out on our own

--- Project2/transaction_server/src/main/java/locking/Lock.java
+++ Project2/transaction_server/src/main/java/locking/Lock.java
@@ -1,5 +1,8 @@
 package locking;
 
+import java.util.Vector;
+import transaction.TransID;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,5 +15,39 @@ package locking;
  */
 public class Lock 
 {
+    private LockType lockType;
+    private Vector holders;
+    private Object object;
     //Has wait and notify
+    
+    //has function "aquire"
+    public synchronized void aquire(TransID trans, LockType aLockType)
+    {
+        //while another transaction holds the lock in confilciting mode
+        while(false /*STUB*/)
+        {
+            try
+            {
+                wait();
+            }
+            catch(InterruptedException e)
+            {
+            }
+        }
+        
+        if(holders.isEmpty())
+        {
+            holders.addElement(trans);
+            lockType = aLockType;
+        } else if(true /*STUB*/) //if another transaction holds a lock then share it
+                {
+                }
+    }
+    
+    public synchronized void release(TransID trans)
+    {
+        holders.removeElement(trans);
+        //set LockType to None
+        notifyAll();
+    }
 }
--- Project2/transaction_server/src/main/java/locking/LockManager.java
+++ Project2/transaction_server/src/main/java/locking/LockManager.java
@@ -1,5 +1,9 @@
 package locking;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+import transaction.TransID;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,6 +16,8 @@ package locking;
  */
 public class LockManager 
 {
+    private Hashtable lockList;
+    //ALTERNATIVE ==> Have each transaction hold its own locks
     // pseudo code in the book
 	
     //Handles the demands for a lock and their release and aquisition
@@ -20,4 +26,28 @@ public class LockManager
     High level holds 
 			Set lock / release lock
     */
+    public void setLock(Object object, TransID trans, LockType lockType)
+    {
+        Lock foundLock = null;
+        synchronized (this) 
+        {
+            // find lock associated with the object
+            // if there isn't one  create it and add it to the hashtable
+        }
+        foundLock.aquire(trans, lockType);
+    }
+    
+    // Used for close transaction 
+    public synchronized void unLock(TransID trans)
+    {
+        Enumeration e = lockList.elements();
+        while(e.hasMoreElements())
+        {
+            Lock aLock = (Lock) (e.nextElement());
+            if(true /*STUB trans is a holder of this lock */)
+            {
+                aLock.release(trans);
+            }
+        }
+    }
 }
