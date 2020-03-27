7dbde2b716dc72812eef37a2dd9f0d61bee643e0
kennyklaw <47403283+kennyklaw@users.noreply.github.com>
2020-03-27 13:06:06 -0700

    added code to transaction

--- Project2/transaction_server/src/main/java/transaction/Transaction.java
+++ Project2/transaction_server/src/main/java/transaction/Transaction.java
@@ -11,6 +11,49 @@ package transaction;
  */
 public class Transaction 
 {
+    int transID;
+    ArrayList<Lock> locks = null;
+    
+    StringBuffer log = new StringBuffer("");
+    
+    Transaction(int transID)
+    {
+        this.transID = transID;
+        this.locks = new ArrayList();
+    }
+    
+    public int getID()
+    {
+        return transID;
+    }
+    
+    public ArrayList<Lock> getLocks()
+    {
+        return locks;
+    }
+    
+    public void addLock(Lock lock)
+    {
+        locks.add(lock);
+    }
+    
+    public void log (String logString)
+    {
+        log.append("\n).append(logString);
+        
+        if (!TransactionSErver.transactionView)
+        {
+            System.out.println("Transaction # " + this.getID() + " " + logString);
+        }
+     }
+     
+     public StringBuffer getLog() 
+     {
+         return log;
+     }
+                   
+ }
+                       
     // note from otte
         // make transaction a bit more elaborate attach messages to the transactions
         // put comments with transactions themselves
@@ -18,4 +61,4 @@ public class Transaction
     //Holds a reference to the account it wants to perform a transaction
     //Defines itself as Read OR Write
     //POSSIBILITY ==> Holds its own locks, suggested by Otte
-}
+
