091edf14f9902469bc8adb1907b6f445a69f3cd0
CalebJEJ <cjj226@nau.edu>
2020-03-29 15:25:29 -0700

    Added code to the main
    Main is now set to create the transaction server and a set number of clients that will perfrom a number of random transactions

--- Project2/transaction_server/src/main/java/transaction_server/Main.java
+++ Project2/transaction_server/src/main/java/transaction_server/Main.java
@@ -5,6 +5,8 @@
  */
 package transaction_server;
 
+import client.Client;
+
 /**
  *
  * @author caleb
@@ -15,12 +17,27 @@ public class Main
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        int numTestAccounts = 3; //Number of the accounts in the system
+        int maxBalance = 500; //Max balance an account starts at (decides highest possible random transaction value)
+        int numTransactions = 3; // Number of random transactons to perform
+        int numClients = 3; // Number of client threads made
+        int startPort = 2080; //Port number to start at to give client
+        String defaultHost = "127.0.0.1"; //default ip of the testing host for the clients
+        
         // Create a new Transaction Server
-        
+        TransactionServer tranServer = new TransactionServer();
         // Create a new Client and proxy //THREAD Client to have multiple clients at same time
-        
-        // Have Client issue some commands to test the system
+        for(int i; i < numClients; i++)
+        {
+            Client newClient = new Client(defaultHost,
+                    startPort + i,
+                    numTestAccounts,
+                    maxBalance,
+                    numTransactions);
+            newClient.start();
+        }
     }
     
 }
