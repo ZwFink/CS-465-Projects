2208eaec897ace8ab7b62f68bf31a750da6a2401
CalebJEJ <cjj226@nau.edu>
2020-03-30 14:30:29 -0700

    Fiexed end display for clarity
    race conditons still cause ether money

--- Project2/transaction_server/src/main/java/transaction_server/TransactionServer.java
+++ Project2/transaction_server/src/main/java/transaction_server/TransactionServer.java
@@ -37,6 +37,7 @@ public class TransactionServer extends Thread
     public int numClients;
     public int numTransactions;
     public static boolean transactionView = true;
+    public ArrayList<Socket> clientList = new ArrayList();
     //Creates a server socket
 
     public TransactionServer(String host, int port)
@@ -102,6 +103,7 @@ public class TransactionServer extends Thread
                 System.out.println("Server failed to accept client.");
                 continue;
             }
+            clientList.add(client);
             System.out.println("Connection Established");
 
             //Got a client, give them to a new worker thread and go back to waiting for a new client
@@ -110,34 +112,24 @@ public class TransactionServer extends Thread
             
             handledClients++;
         }
+        
+        //Waiting loop to make sure all clients close before server does
+        boolean clientsRunning = true;
+        while(clientsRunning)
+        {
+            clientsRunning = false;
+            for(Socket client: clientList)
+            {
+                if(!client.isClosed())
+                {
+                    clientsRunning = true;
+                }
+            }
+        }
+        
         ArrayList<Transaction> transactions = transMan.getTransactions();
         int numTrans = transactions.size();
         System.out.println( "Total Transactions: " + numTrans );
-        /**/
-        boolean transRemaining = true;
-        while(transRemaining)
-        {
-            synchronized( transactions )
-                {
-                    transRemaining = false;
-                    for(Transaction tran : transactions)
-                    {
-                        if(!tran.getState())
-                        {
-                            transRemaining = true;
-                        }
-                        
-                    }
-                }
-        }
-        
-        synchronized( transactions )
-        {
-            for(Transaction tran : transactions)
-            {
-                System.out.println(tran.getLog());
-            }
-        }
         
 
         int endingSum = accMan.getBalanceSum();
