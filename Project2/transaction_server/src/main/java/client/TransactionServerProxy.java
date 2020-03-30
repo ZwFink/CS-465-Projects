a95ed14f2b2dff7d60874ab289e253e3c13e8854
kennyklaw <47403283+kennyklaw@users.noreply.github.com>
2020-03-30 01:14:39 -0700

    updated proxy

--- Project2/transaction_server/src/main/java/client/TransactionServerProxy.java
+++ Project2/transaction_server/src/main/java/client/TransactionServerProxy.java
@@ -5,18 +5,26 @@
  */
 package client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import transaction.comm.Message;
+
+
 /**
  * Shields Client from Server
  * Acts on behalf of Client
  * @author caleb
  */
-public class TransactionServerProxy 
+public class TransactionServerProxy
 {
     private Socket socket = null;
     private ObjectOutputStream writeTo = null;
     private ObjectInputStream readFrom = null;
     private String host;
     private int port;
+    private int transID = 0;
     
     TransactionServerProxy(String host, int port) 
     {
@@ -38,47 +46,59 @@ public class TransactionServerProxy
             writeTo = new ObjectOutputStream(socket.getOutputStream());
             
             // need to handle messages
+            Message openTrans = new Message(OPEN_TRANSACTION);
         }
-        catch
+        catch(IOException e)
         {
             System.out.println("Unable to open transaction");
         }
+        
+        return transID;
     }
 
     int read(int accountFrom) 
     {
         try
-        {
-           
-            
+        {    
             // need to handle messages
+            Message readRequest = new Message(READ_REQUEST);
         }
-        catch
+        catch(IOException e)
         {
             System.out.println("Unable to read transaction");
-        }.
+        }
+        
+        return balance;
     }
 
-    void write(int accountTo, int toBalance) {
+    void write(int accountTo, int toBalance) 
+    {
         try
-        {
-           
+        {    
             // need to handle messages
+            Message writeRequest = new Message(WRITE_REQUEST);
         }
-        catch
+        catch(IOException e)
         {
             System.out.println("Unable to write transaction");
         }
+        
+        return balance;
     }
 
-    void closeTransaction() {
+    void closeTransaction() 
+    {
         try
-        {
-           
-            
+        {       
             // need to handle messages
+            Message closeTrans = new Message(CLOSE_TRANSACTION);
+            
+            writeTo.writeObject(closeMessage);
+            
+            //close the connection
+            socket.close();
         }
-        catch
+        catch(IOException e)
         {
             System.out.println("Unable to close transaction");
         }
